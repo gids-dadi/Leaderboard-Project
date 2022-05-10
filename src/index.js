c20a8250134f17b4d8c8fcd486bd123d0b6e4f86
gids-dadi <gideons564@gmail.com>
2022-05-10 21:00:09 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import _ from 'lodash';
 import './style.css';
 
-// const title = document.createElement('h3');
-// title.textContent = 'Weback is easyyyyyyyyyyy!!!!! ';
-
-// const page = document.querySelector('body');
-// page.append(title);
