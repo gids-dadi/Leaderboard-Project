826b4080a3b105515daefa1ff65e4c11357d890b
gids-dadi <gideons564@gmail.com>
2022-05-10 20:40:17 +0100

    Add setup file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+import './style.css';
+
+// const title = document.createElement('h3');
+// title.textContent = 'Weback is easyyyyyyyyyyy!!!!! ';
+
+// const page = document.querySelector('body');
+// page.append(title);
