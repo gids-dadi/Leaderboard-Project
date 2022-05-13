f9c5ad5cf6db373c4b02668966e9b8a9afe183cf
gids-dadi <gideons564@gmail.com>
2022-05-13 05:59:07 +0100

    fix linter errros

--- src/index.js
+++ src/index.js
@@ -1,38 +1,3 @@
-// import _ from 'lodash';
-// import './style.css';
-
-// import { getScores, postScore } from './scoresLoad';
-// import renderScore from './scoresRender';
-
-// const refBtn = document.querySelector('#refresh');
-// const form = document.querySelector('form');
-// const msg = document.querySelector('#msg');
-
-// refBtn.addEventListener('click', async () => {
-//   const list = await getScores();
-//   renderScore(list);
-// });
-
-// form.addEventListener('submit', async (event) => {
-//   event.preventDefault();
-
-//   const score = {
-//     user: form.elements.user.value,
-//     score: form.elements.score.value,
-//   };
-
-//   form.reset();
-//   await postScore(score);
-
-//   setTimeout(() => {
-//     // msg.innerHTML = 'Your score has been added, press Refresh to see';
-//     msg.innerHTML = `"${score.score}" by "${score.user}" has been added, press Refresh to see`;
-//     setTimeout(() => {
-//       msg.innerHTML = '';
-//     }, 3000);
-//   }, 3000);
-// });
-
 import _ from 'lodash';
 import './style.css';
 
@@ -62,10 +27,9 @@ form.addEventListener('submit', async (event) => {
   await postScore(score);
 
   setTimeout(() => {
-    // msg.innerHTML = 'Your score has been added, press Refresh to see';
     msg.innerHTML = `"${score.score}" by "${score.user}" has been added, press Refresh to see`;
     setTimeout(() => {
       msg.innerHTML = '';
-    }, 3000);
-  }, 3000);
+    }, 2000);
+  }, 2000);
 });
--- src/scoresLoad.js
+++ src/scoresLoad.js
@@ -1,14 +1,11 @@
 const gameID = 'Zl4d7IVkemOTTVg2fUdz';
 
-// const baseURL =
-//  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-
 const postScore = async (score) => {
   const response = await fetch(
     `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
     {
       method: 'POST',
-      // mode: 'same-origin',
+      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json',
       },
@@ -16,16 +13,13 @@ const postScore = async (score) => {
     }
   );
   return response.json();
-  // return result;
 };
 
 const getScores = async () => {
   const response = await fetch(
     `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`
   );
-  // const { result } = await
   return response.json();
-
-  // return result;
 };
+
 export { getScores, postScore };
